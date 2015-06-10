ed0760aab9bc05391144bf9958cb11595428616a
Administrateur Platon <osadmin@in-ntv-tst-04.novalocal>
2015-06-10 16:58:05 +0200

    Add CloudWatt scripting

---
+++ cloudwatt/create.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "### nova boot "
+nova boot --flavor t1.cw.tiny --image "CentOS 6.5" --key-name KP_BadCops-Dev_GLL --security-groups SSH_Only,SGrpAdmin --user_data user_data_file.yaml IN-BAD-DEV1-GLL-01
+echo "### sleep "
+sleep 30
+
+echo "### nova floating-ip-associate "
+nova floating-ip-associate IN-BAD-DEV1-GLL-01 84.39.35.168
+
+echo "### nova show "
+nova show IN-BAD-DEV1-GLL-01
+
+sleep 10
+
+echo "### nova floating-ip-associate "
+nova floating-ip-associate IN-BAD-DEV1-GLL-01 84.39.35.168
+
+echo "### nova show "
+nova show IN-BAD-DEV1-GLL-01
+
